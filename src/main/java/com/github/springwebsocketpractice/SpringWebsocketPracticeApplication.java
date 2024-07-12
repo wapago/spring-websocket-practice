c7ff6a720d083b4caca5c69cc90beb9bf9b259d3
Kim Chanho <beroso20th@gmail.com>
2024-07-12 23:43:53 +0900

    first commit

---
+++ src/main/java/com/github/springwebsocketpractice/SpringWebsocketPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.springwebsocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springwebsocketpractice/SpringWebsocketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springwebsocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
