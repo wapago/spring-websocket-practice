b35f4ae1b2cee97c45382f3f33f3ab4bcb54e6f0
Kim Chanho <beroso20th@gmail.com>
2024-07-13 23:29:37 +0900

    Feat: 간단한 채팅

---
+++ src/main/java/com/github/springwebsocketpractice/Greeting.java
@@ -0,0 +1,12 @@
+package com.github.springwebsocketpractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/com/github/springwebsocketpractice/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.github.springwebsocketpractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+    private String name;
+}
